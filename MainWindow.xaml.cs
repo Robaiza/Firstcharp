8ff65f146bec9b53a995a94059167df1434729e2
Robaiza <robbaiza@yahoo.com>
2022-09-28 17:12:30 -0600

    First commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using LiveCharts;
+using LiveCharts.Wpf;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using TabLenguajesProgra.Pantallas;
+
+namespace Tabs
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        ProgramasEntretenimiento programasEntretenimiento = new ProgramasEntretenimiento();
+        LenguajesProgramacion lenguajesProgramacion = new LenguajesProgramacion();
+        Libros libros = new Libros(); Page1 Page1 = new Page1();
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            MyFrame.NavigationService.Navigate(programasEntretenimiento);
+
+            MyFrame.NavigationService.Navigate(Page1);
+        }
+
+
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MyFrame.NavigationService.Navigate(programasEntretenimiento);
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            MyFrame.NavigationService.Navigate(libros);
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            MyFrame.NavigationService.Navigate(lenguajesProgramacion);
+        }
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+            MyFrame.NavigationService.Navigate(Page1);
+        }
+    }
+}
---
+++ Pantallas/LenguajesProgramacion.xaml.cs
@@ -0,0 +1,45 @@
+﻿using LiveCharts.Wpf;
+using LiveCharts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TabLenguajesProgra.Pantallas
+{
+    /// <summary>
+    /// Lógica de interacción para LenguajesProgramacion.xaml
+    /// </summary>
+    public partial class LenguajesProgramacion : Page
+    {
+        public LenguajesProgramacion()
+        {
+            InitializeComponent();
+
+            PointLabel = chartPoint => string.Format("{0}({1:P})", chartPoint.Y, chartPoint.Participation);
+
+            DataContext = this;
+        }
+
+        public Func<ChartPoint, string> PointLabel { get; set; }
+        private void PieChart_DataClick(object sender, LiveCharts.ChartPoint chartPoint)
+        {
+            var chart = (LiveCharts.Wpf.PieChart)chartPoint.ChartView;
+            foreach (PieSeries series in chart.Series)
+                series.PushOut = 0;
+
+            var selectedSeries = (PieSeries)chartPoint.SeriesView;
+            selectedSeries.PushOut = 8;
+        }
+    }
+}
